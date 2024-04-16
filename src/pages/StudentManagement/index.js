c19e12373e3f3b487dbde325856f9e3a0c51c0be
Injun Hwang <ijhwang@whyout.co.kr>
2024-04-17 00:56:10 +0900

    학생 정보 조회

--- src/pages/StudentManagement/index.js
+++ src/pages/StudentManagement/index.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getStudent } from "../../service/apiService";
+import AuthStore from "../../stores/AuthStore";
 
 function StudentManagement() {
-  return <div className="flex h-screen bg-gray-100">StudentManagement</div>;
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    const fetchStudentsData = async () => {
+      const data = await getStudent(AuthStore.user._id);
+      setStudents(data);
+      console.log(data);
+    };
+
+    fetchStudentsData();
+  }, []);
+  // 배열을 문자열로 변환하는 함수
+  const arrayToString = (arr) => arr.join(", ") || "없음";
+
+  return (
+    <div className="flex h-screen bg-gray-100 justify-center items-center">
+      <table className="min-w-full table-fixed text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="py-3 px-6">
+              이름
+            </th>
+            <th scope="col" className="py-3 px-6">
+              성별
+            </th>
+            <th scope="col" className="py-3 px-6">
+              좋아하는 친구
+            </th>
+            <th scope="col" className="py-3 px-6">
+              싫어하는 친구
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((student) => (
+            <tr
+              key={student.id}
+              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+            >
+              <td className="py-4 px-6">{student.name}</td>
+              <td className="py-4 px-6">{student.gender}</td>
+              <td className="py-4 px-6">
+                {arrayToString(student.favorite_friends)}
+              </td>
+              <td className="py-4 px-6">
+                {arrayToString(student.fought_friends)}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default StudentManagement;
--- src/service/apiService.js
+++ src/service/apiService.js
@@ -2,12 +2,15 @@
 import axiosInstance from "./axios"; // axios 인스턴스를 임포트합니다.
 
 export const signup = async (userData) => {
-  console.log(userData);
-  const response = await axiosInstance.post("/user/signup", userData); // baseURL이 이미 설정되어 있으므로, 전체 URL 대신 경로만 지정합니다.
+  const response = await axiosInstance.post("/user/signup", userData);
   return response.data;
 };
 export const signin = async (userData) => {
-  console.log(userData);
-  const response = await axiosInstance.post("/user/signin", userData); // baseURL이 이미 설정되어 있으므로, 전체 URL 대신 경로만 지정합니다.
+  const response = await axiosInstance.post("/user/signin", userData);
+  return response.data;
+};
+export const getStudent = async (teacherId) => {
+  console.log(teacherId);
+  const response = await axiosInstance.get("/students?teacherId=" + teacherId);
   return response.data;
 };

704d0727c8ec6da2bceab8953b6653afc48b4131
Injun <ijhwnag@whout.co.kr>
2024-07-09 16:18:13 +0900

    이미지 등록 처리

--- src/components/StudentListItem.js
+++ src/components/StudentListItem.js
@@ -5,7 +5,15 @@ const StudentListItem = ({ student, onClick }) => (
     className="bg-[#D9D9D9] rounded-lg shadow-md p-4 flex items-center cursor-pointer hover:shadow-lg"
     onClick={() => onClick(student)}
   >
-    <div className="w-16 h-16 bg-gray-400 rounded-full"></div>
+    {student.imageUrl ? (
+      <img
+        src={student.imageUrl}
+        alt={`${student.name} 프로필`}
+        className="w-16 h-16 object-cover rounded-full"
+      />
+    ) : (
+      <div className="w-16 h-16 bg-gray-400 rounded-full mx-auto"></div>
+    )}
     <div className="ml-4">
       <h2 className="text-lg font-semibold">{student.name}</h2>
       <p className="text-sm text-gray-600">{student.gender}</p>

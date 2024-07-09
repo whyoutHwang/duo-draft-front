50b3947840d6377fed66ffeb79c7cbc22247da95
Injun <ijhwnag@whout.co.kr>
2024-07-09 16:17:51 +0900

    로그인 회원가입 페이지 마무리

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import authStore from "../../stores/AuthStore";
 import { observer } from "mobx-react";
 
 import { signin } from "../../service/apiService";
+
+import Yellow from "../../assets/image/character/Yellow.png";
+import Red from "../../assets/image/character/Red.png";
+import Green from "../../assets/image/character/Green.png";
+import Pink1 from "../../assets/image/character/Pink-1.png";
+import LightGreen from "../../assets/image/character/Light Green.png";
+import Skin from "../../assets/image/character/Skin.png";
+import Yellow2 from "../../assets/image/character/Yellow_2.png";
+import LightPurple from "../../assets/image/character/Light Purple.png";
+import Mint from "../../assets/image/character/Mint.png";
+import LightBrown from "../../assets/image/character/Light Brown.png";
+import Purple from "../../assets/image/character/Purple.png";
+import Gray from "../../assets/image/character/Gray.png";
+import Orange from "../../assets/image/character/Orange.png";
+import Pink from "../../assets/image/character/Pink.png";
+import LOGO from "../../assets/image/logo/logo.svg";
 
 function Signin() {
   const [formData, setFormData] = useState({
@@ -12,9 +28,38 @@ function Signin() {
   });
   const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 함수를 생성합니다
 
+  // 캐릭터 배열
+  const characters = [
+    { src: Yellow, alt: "Yellow Character" },
+    { src: Red, alt: "Red Character" },
+    { src: Green, alt: "Green Character" },
+    { src: Pink1, alt: "Pink Character 1" },
+    { src: LightGreen, alt: "Light Green Character" },
+    { src: Skin, alt: "Skin Character" },
+    { src: Yellow2, alt: "Yellow Character 2" },
+    { src: LightPurple, alt: "Light Purple Character" },
+    { src: Mint, alt: "Mint Character" },
+    { src: LightBrown, alt: "Light Brown Character" },
+    { src: Purple, alt: "Purple Character" },
+    { src: Gray, alt: "Gray Character" },
+    { src: Orange, alt: "Orange Character" },
+    { src: Pink, alt: "Pink Character" },
+  ];
+
   const handleChange = (event) => {
     setFormData({ ...formData, [event.target.name]: event.target.value });
   };
+
+  const getRandomPosition = () => ({
+    top: `${Math.random() * 80}%`,
+    left: `${Math.random() * 80}%`,
+    transform: `rotate(${Math.random() * 360}deg)`,
+  });
+
+  // 캐릭터 위치를 메모이제이션
+  const characterPositions = useMemo(() => {
+    return characters.map(() => getRandomPosition());
+  }, []); // 빈 의존성 배열로 컴포넌트 마운트 시 한 번만 실행
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -34,10 +79,23 @@ function Signin() {
 
   return (
     <div className="min-h-screen flex">
+      {characters.map((char, index) => (
+        <img
+          key={index}
+          src={char.src}
+          alt={char.alt}
+          className="absolute w-32 h-32 object-contain"
+          style={characterPositions[index]}
+        />
+      ))}
       <div className="flex-1 flex items-center justify-center ">
-        <div className="max-w-md w-full bg-[#F2EFE8] p-10 rounded-2xl shadow-md">
-          <div>
-            <span className="mt-6 font-extrabold text-gray-900">DUO DRAFT</span>
+        <div
+          className="max-w-md w-full bg-[#F2EFE8] p-10 rounded-2xl shadow-md"
+          style={{ zIndex: 10 }}
+        >
+          <div className="flex items-center">
+            <img src={LOGO} alt={"logo"} className="w-8 h-8 object-contain" />
+            <span className="ml-4 font-extrabold text-gray-900">로그인</span>
           </div>
           <form className="mt-8 space-y-6">
             <input type="hidden" name="remember" value="true" />
@@ -51,7 +109,7 @@ function Signin() {
                   onChange={handleChange}
                   autoComplete="email"
                   required
-                  className="mt-2 appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  className="mt-2 appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-[#397358] focus:border-[#397358] sm:text-sm"
                   placeholder="이메일 주소"
                 />
               </div>
@@ -65,7 +123,7 @@ function Signin() {
                   onChange={handleChange}
                   autoComplete="current-password"
                   required
-                  className="mt-2 appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  className="mt-2 appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-[#397358] focus:border-[#397358] sm:text-sm"
                   placeholder="비밀번호"
                 />
               </div>
@@ -74,7 +132,7 @@ function Signin() {
             <div className="mt-6">
               <button
                 onClick={handleSubmit}
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#397358] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#397358] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#397358]"
               >
                 로그인
               </button>
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,8 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import Modal from "react-modal";
 import { useNavigate } from "react-router-dom";
 import { signup } from "../../service/apiService";
 import styles from "./Signup.module.css";
+
+import Yellow from "../../assets/image/character/Yellow.png";
+import Red from "../../assets/image/character/Red.png";
+import Green from "../../assets/image/character/Green.png";
+import Pink1 from "../../assets/image/character/Pink-1.png";
+import LightGreen from "../../assets/image/character/Light Green.png";
+import Skin from "../../assets/image/character/Skin.png";
+import Yellow2 from "../../assets/image/character/Yellow_2.png";
+import LightPurple from "../../assets/image/character/Light Purple.png";
+import Mint from "../../assets/image/character/Mint.png";
+import LightBrown from "../../assets/image/character/Light Brown.png";
+import Purple from "../../assets/image/character/Purple.png";
+import Gray from "../../assets/image/character/Gray.png";
+import Orange from "../../assets/image/character/Orange.png";
+import Pink from "../../assets/image/character/Pink.png";
+import LOGO from "../../assets/image/logo/logo.svg";
 
 Modal.setAppElement("#root");
 
@@ -17,6 +33,35 @@ function Signup() {
   const [modalTitle, setModalTitle] = useState("");
   const [modalContent, setModalContent] = useState("");
   const navigate = useNavigate();
+
+  // 캐릭터 배열
+  const characters = [
+    { src: Yellow, alt: "Yellow Character" },
+    { src: Red, alt: "Red Character" },
+    { src: Green, alt: "Green Character" },
+    { src: Pink1, alt: "Pink Character 1" },
+    { src: LightGreen, alt: "Light Green Character" },
+    { src: Skin, alt: "Skin Character" },
+    { src: Yellow2, alt: "Yellow Character 2" },
+    { src: LightPurple, alt: "Light Purple Character" },
+    { src: Mint, alt: "Mint Character" },
+    { src: LightBrown, alt: "Light Brown Character" },
+    { src: Purple, alt: "Purple Character" },
+    { src: Gray, alt: "Gray Character" },
+    { src: Orange, alt: "Orange Character" },
+    { src: Pink, alt: "Pink Character" },
+  ];
+
+  const getRandomPosition = () => ({
+    top: `${Math.random() * 80}%`,
+    left: `${Math.random() * 80}%`,
+    transform: `rotate(${Math.random() * 360}deg)`,
+  });
+
+  // 캐릭터 위치를 메모이제이션
+  const characterPositions = useMemo(() => {
+    return characters.map(() => getRandomPosition());
+  }, []); // 빈 의존성 배열로 컴포넌트 마운트 시 한 번만 실행
 
   const handleChange = (event) => {
     const { name, value, type, checked } = event.target;
@@ -50,31 +95,61 @@ function Signup() {
     setModalContent("");
   };
 
+  const randomCharacter = useMemo(() => {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    return characters[randomIndex];
+  }, []);
+
   return (
     <div className="min-h-screen flex">
-      <div className="flex-1 flex items-center justify-center ">
+      {characters.map((char, index) => (
+        <img
+          key={index}
+          src={char.src}
+          alt={char.alt}
+          className="absolute w-32 h-32 object-contain"
+          style={characterPositions[index]}
+        />
+      ))}
+      <div
+        className="flex-1 flex items-center justify-center "
+        style={{ zIndex: 10 }}
+      >
         <div className="max-w-lg w-full bg-[#F2EFE8] p-10 rounded-2xl shadow-md">
-          <div>
-            <span className="mt-6 font-extrabold text-gray-900">DUO DRAFT</span>
-          </div>
           {signupSuccess ? (
             <div className="text-center">
-              <div style={{ width: "300px", height: "300px" }}></div>
+              <div
+                style={{ width: "300px", height: "300px" }}
+                className="mx-auto"
+              >
+                <img
+                  src={randomCharacter.src}
+                  alt={randomCharacter.alt}
+                  className="w-full h-full object-contain"
+                />
+              </div>
               <p className="mt-4">회원가입 완료!</p>
               <button
                 onClick={() => navigate("/signin")}
-                className="mt-6 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#397358] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="mt-6 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#397358] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#397358]"
               >
                 로그인 페이지로 이동
               </button>
             </div>
           ) : (
-            <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-              <div className="mt-8">
-                <h4>회원가입</h4>
+            <form className="space-y-6 " onSubmit={handleSubmit}>
+              <div className="flex items-center">
+                <img
+                  src={LOGO}
+                  alt={"logo"}
+                  className="w-8 h-8 object-contain"
+                />
+                <span className="ml-4 font-extrabold text-gray-900">
+                  회원가입
+                </span>
               </div>
               <input type="hidden" name="remember" value="true" />
-              <div className="rounded-md shadow-sm -space-y-px ">
+              <div className="rounded-md shadow-sm">
                 <div className="mb-4">
                   <label htmlFor="email-address">이메일 주소</label>
                   <input
@@ -84,11 +159,11 @@ function Signup() {
                     onChange={handleChange}
                     autoComplete="email"
                     required
-                    className="mt-2 appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    className="mt-2 appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-[#397358] focus:border-[#397358] sm:text-sm"
                     placeholder="이메일 주소"
                   />
                 </div>
-                <div>
+                <div className="mb-4">
                   <label htmlFor="password">비밀번호</label>
                   <input
                     id="password"
@@ -97,11 +172,11 @@ function Signup() {
                     onChange={handleChange}
                     autoComplete="current-password"
                     required
-                    className="mt-2 appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    className="mt-2 appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-[#397358] focus:border-[#397358] sm:text-sm"
                     placeholder="비밀번호"
                   />
                 </div>
-                <div>
+                <div className="mb-4">
                   <label htmlFor="password-confirm">비밀번호 확인하기</label>
                   <input
                     id="password-confirm"
@@ -110,16 +185,25 @@ function Signup() {
                     onChange={handleChange}
                     autoComplete="current-password"
                     required
-                    className="mt-2 appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    className="mt-2 appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-[#397358] focus:border-[#397358] sm:text-sm"
                     placeholder="비밀번호"
                   />
                 </div>
               </div>
               <div className="mt-8">
-                <h4>약관동의</h4>
+                <div className="flex items-center">
+                  <img
+                    src={LOGO}
+                    alt={"logo"}
+                    className="w-8 h-8 object-contain"
+                  />
+                  <span className="ml-4 font-extrabold text-gray-900">
+                    약관동의
+                  </span>
+                </div>
                 <div className="mt-4">
                   <label className="flex justify-between items-center">
-                    <div className="flex">
+                    <div className="flex items-center">
                       <input
                         type="checkbox"
                         name="terms1"
@@ -144,7 +228,7 @@ function Signup() {
                 </div>
                 <div className="mt-4">
                   <label className="flex justify-between items-center">
-                    <div className="flex">
+                    <div className="flex items-center">
                       <input
                         type="checkbox"
                         name="terms2"
@@ -177,7 +261,7 @@ function Signup() {
               <div className="mt-6">
                 <button
                   type="submit"
-                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#397358] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#397358] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#397358]"
                 >
                   모두 동의하고 시작하기
                 </button>
@@ -201,7 +285,7 @@ function Signup() {
           <div className="text-gray-700">{modalContent}</div>
           <button
             onClick={closeModal}
-            className="mt-4 w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#397358] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="mt-4 w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#397358] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#397358]"
           >
             닫기
           </button>
